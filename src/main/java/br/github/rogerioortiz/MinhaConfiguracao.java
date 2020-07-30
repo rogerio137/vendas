e32439d5c7c68a003c417ca35e365dfc8abbcbe1
Rogerio Barrucalle Ortiz <rortiz@ciandt.com>
2020-07-30 14:05:57 -0300

    Iniciando o projeto de estudos de spring boot

---
+++ src/main/java/br/github/rogerioortiz/MinhaConfiguracao.java
@@ -0,0 +1,13 @@
+package br.github.rogerioortiz;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinhaConfiguracao {
+
+	@Bean(name = "applicationName")
+	public String applicationName(){
+		return "Sistema de Vendas";
+	}
+}
