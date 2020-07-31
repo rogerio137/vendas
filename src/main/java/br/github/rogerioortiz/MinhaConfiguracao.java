ee00e4a8ae0fdf0209b79b3e5eaaeb2bc35904eb
Rogerio Barrucalle Ortiz <rortiz@ciandt.com>
2020-07-31 09:39:18 -0300

    Trabalhando com multiplos ambientes

--- src/main/java/br/github/rogerioortiz/MinhaConfiguracao.java
+++ src/main/java/br/github/rogerioortiz/MinhaConfiguracao.java
@@ -1,13 +1,18 @@
 package br.github.rogerioortiz;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class MinhaConfiguracao {
 
-	@Bean(name = "applicationName")
-	public String applicationName(){
-		return "Sistema de Vendas";
+	@Bean
+	public CommandLineRunner executar(){
+		return args -> {
+			System.out.println("RODANDO A CONFIGURAÇÃO DE DESENVOLVIMENTO");
+		};
 	}
 }
