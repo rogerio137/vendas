a553aaf1ba232a9a5e1b6d30f5554926a085da37
Rogerio <rogerio.137@bol.com.br>
2020-08-18 15:05:02 -0300

    Salvando e recuperando clientes

--- src/main/java/br/github/rogerioortiz/VendasApplication.java
+++ src/main/java/br/github/rogerioortiz/VendasApplication.java
@@ -1,14 +1,35 @@
 package br.github.rogerioortiz;
 
+import br.github.rogerioortiz.domain.entity.Cliente;
+import br.github.rogerioortiz.domain.repository.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 
 @SpringBootApplication
 
 public class VendasApplication {
 
-		public static void main(String[] args) {
+	@Bean
+	public CommandLineRunner init(@Autowired Clientes clientes){
+		return args -> {
+			clientes.salvar(new Cliente("Rogerio Barrucalle Ortiz"));
+			clientes.salvar(new Cliente("Outro cliente"));
+
+			List<Cliente> todosClientes = clientes.obterTodos();
+			todosClientes.forEach(System.out::println);
+
+		};
+	}
+
+
+
+	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
 	}
 }
