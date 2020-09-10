7be1937facd2f57812a656b68b6766b8b5280554
Rogerio <barrucalleortiz@gmail.com>
2020-09-10 09:47:40 -0300

    Persistindo entidades com Entity Manager

--- src/main/java/br/github/rogerioortiz/VendasApplication.java
+++ src/main/java/br/github/rogerioortiz/VendasApplication.java
@@ -25,7 +25,7 @@ public class VendasApplication {
 			List<Cliente> todosClientes = clientes.obterTodos();
 			todosClientes.forEach(System.out::println);
 
-			System.out.println("Atualizando clientes");
+			/*System.out.println("Atualizando clientes");
 			todosClientes.forEach(c -> {
 				c.setNome(c.getNome() + " atualizado.");
 				clientes.atualizar(c);
@@ -37,17 +37,17 @@ public class VendasApplication {
 			System.out.println("Buscando clientes");
 			clientes.buscarPorNome("Cli").forEach(System.out::println);
 
-//            System.out.println("deletando clientes");
-//            clientes.obterTodos().forEach(c -> {
-//                clientes.deletar(c);
-//            });
+            System.out.println("deletando clientes");
+            clientes.obterTodos().forEach(c -> {
+                clientes.deletar(c);
+            });
 
 			todosClientes = clientes.obterTodos();
 			if(todosClientes.isEmpty()){
 				System.out.println("Nenhum cliente encontrado.");
 			}else{
 				todosClientes.forEach(System.out::println);
-			}
+			}*/
 		};
 	}
 
