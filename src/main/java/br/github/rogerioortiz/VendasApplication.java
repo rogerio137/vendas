43480436e7e05595a3706c75c81459965d92e319
Rogerio <barrucalleortiz@gmail.com>
2020-09-11 08:47:22 -0300

    Requisição GET com parâmetros e Response Entity

--- src/main/java/br/github/rogerioortiz/VendasApplication.java
+++ src/main/java/br/github/rogerioortiz/VendasApplication.java
@@ -1,11 +1,24 @@
 package br.github.rogerioortiz;
 
+import br.github.rogerioortiz.domain.entity.Cliente;
+import br.github.rogerioortiz.domain.repository.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
 public class VendasApplication {
+
+	@Bean
+	public CommandLineRunner commandLineRunner(@Autowired Clientes clientes){
+		return args -> {
+			Cliente c = new Cliente(null, "Fulano");
+			clientes.save(c);
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
