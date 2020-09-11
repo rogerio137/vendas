51e027ca5816283f92d2fb4056693a8fbedc1ade
Rogerio <barrucalleortiz@gmail.com>
2020-09-11 14:32:23 -0300

    Refatorando utilizando Projeto Lombok

---
+++ src/main/java/br/github/rogerioortiz/rest/dto/ItemPedidoDTO.java
@@ -0,0 +1,15 @@
+package br.github.rogerioortiz.rest.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemPedidoDTO {
+    private Integer produto;
+    private Integer quantidade;
+
+}
