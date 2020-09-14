517f015ed2fb96299afb5827b1075bf08b9e0925
Rogerio <barrucalleortiz@gmail.com>
2020-09-14 16:48:24 -0300

    Utilizando e testando o Bean Validation

--- src/main/java/br/github/rogerioortiz/domain/entity/Cliente.java
+++ src/main/java/br/github/rogerioortiz/domain/entity/Cliente.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.Set;
 
 
@@ -22,6 +23,7 @@ public class Cliente {
     private Integer id;
 
     @Column(name = "nome", length = 100)
+    @NotEmpty(message = "Campo nome é obrigatório.")
     private String nome;
 
     @Column(name = "cpf", length = 11)
