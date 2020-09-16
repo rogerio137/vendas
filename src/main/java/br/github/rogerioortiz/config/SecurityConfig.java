0b0bc59009d4e8b0ab06e182cc2f681eb26a39a8
Rogerio <barrucalleortiz@gmail.com>
2020-09-16 11:04:20 -0300

    Testando a autenticação com cadastro de usuario

--- src/main/java/br/github/rogerioortiz/config/SecurityConfig.java
+++ src/main/java/br/github/rogerioortiz/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package br.github.rogerioortiz.config;
 import br.github.rogerioortiz.service.impl.UsuarioServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -39,6 +40,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .hasRole("ADMIN")
                     .antMatchers("/api/pedidos/**")
                         .hasAnyRole("USER", "ADMIN")
+                    .antMatchers(HttpMethod.POST, "/api/usuarios/**")
+                        .permitAll()
+                    .anyRequest().authenticated()
                 .and()
                     .httpBasic();
     }
